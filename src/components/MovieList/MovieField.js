03932eb356512345781ec6e68dafb1d7b769b65d
Jeferson-Mendes <jefersonmendes3c@gmail.com>
2020-06-23 10:38:06 -0300

    some details

--- src/components/MovieList/MovieField.js
+++ src/components/MovieList/MovieField.js
@@ -7,9 +7,9 @@ import './style.css'
 const MovieField = (props) => {
     return (
         <div className="movie-card">
-            <img className="movie-figure" src={ props.image ? `https://image.tmdb.org/t/p/w500/${props.image}` : no_image } alt="CardImage"/>
+        <Link to={`/detail/${props.movieId}`}> <img className="movie-figure" src={ props.image ? `https://image.tmdb.org/t/p/w500/${props.image}` : no_image } alt="CardImage"/> </Link>
             <div className="details">
-                <h3>{props.movieTitle}</h3>
+            <Link to={`/detail/${props.movieId}`} > <h3>{props.movieTitle}</h3> </Link>
                 <h4>{props.release}</h4>
                <Link to={`/detail/${props.movieId}`} > <p>Show details</p> </Link>
             </div>
